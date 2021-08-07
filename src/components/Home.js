267e29452fe1ff858dbf932985eef88a688702ed
Varun Sharma <varunsh170502@gmail.com>
2021-08-08 00:17:20 +0530

    update4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import "./home.css"
-import Typical from 'react-typical'
+// import Typical from 'react-typical'
 function Home() {
     return (
         <>
 
-        <div className="homeall">
-            <div className="home" id="home">
-                <div className="home-content">
-                    <div className="text">
-                        <div className="text-one">Hello,</div>
-                        <div className="text-two">I'm Varun Sharma</div>
-                        <div className="text-three">
-                        <Typical
+            <div className="homeall">
+                <div className="home" id="home">
+                    <div className="home-content">
+                        <div className="text">
+                            <div className="text-one">Hello,</div>
+                            <div className="text-two">I'm Varun Sharma</div>
+                            <div className="text-three">
+                                {/* <Typical
         steps={['Frontend', 3000,'React js',3000,'MERN',3000,'Python','Mongo DB',3000]}
         loop={Infinity}
         wrapper="p"
-      />
-                        </div>
-                        
+      /> */}
+                            </div>
 
-                    </div>
-                    <div className="button">
-                        <button>Hire Me</button>
+
+                        </div>
+                        <div className="button">
+                            <button>Hire Me</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
-        
+
     )
 }
 
